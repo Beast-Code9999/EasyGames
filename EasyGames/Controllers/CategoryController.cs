3fc5afa660428244f58f283d0509d9421213ceb3
Beast Code <beastcode9999@gmail.com>
2025-09-14 14:40:23 +0930

    Add Category controller Add Category views folder and index Add Category link on _Layout

---
+++ EasyGames/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyGames.Controllers
+{
+    public class CategoryController : Controller 
+    {
+        public IActionResult Index() // action is Index
+        {
+            return View(); :wq
+        }
+    }
+}
