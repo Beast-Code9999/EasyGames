4b54006f6657bb0292015f9b424e3918b2658cc8
Beast Code <beastcode9999@gmail.com>
2025-09-14 12:15:24 +0930

    added Category.cs

---
+++ EasyGames/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace EasyGames.Models
+{
+    public class Category
+    {
+        
+        [Key] // Explicitly define Id as the primary key
+        public int Id { get; set; }
+        [Required] // Ensure that the script in the SQL have a not null setting
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
