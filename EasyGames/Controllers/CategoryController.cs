09c3ab4ffdbbd96dbd4a86d9003a4fa67b9a1621
Beast Code <beastcode9999@gmail.com>
2025-09-14 15:09:28 +0930

    Add list of categories in controller

--- EasyGames/Controllers/CategoryController.cs
+++ EasyGames/Controllers/CategoryController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyGames.Data;
+using EasyGames.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyGames.Controllers
 {
     public class CategoryController : Controller 
     {
+        // with Dotnet core we do not have to use legacy code such as
+        // ApplicationDbContext name = new ApplicationDbContext();
+
+        // Uses Dependency Injection to access the database via ApplicationDbContext
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db )
+        {
+            _db = db;
+        }
         public IActionResult Index() // action is Index
         {
+            // Retrieves all categories from the database as a list
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
